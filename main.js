ba81b037aee3a3e34d089a7deb726e7ddaa4316d
Iona Erofeeff <37254166+ionarevamp@users.noreply.github.com>
2025-04-07 11:37:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,17 @@ function addDiv(id, text) {
 
   parentElement.appendChild(newElement);
 }
+function setTextById(id, text) {
+  const parentElement = document.getElementById(id);
+  parentElement.textContent = text;
+}
 
 //current_doc = document.body.innerHTML;
 //document.body.innerHTML = current_doc + "HELLO!"
 addDiv("test", "HELLO!?");
+
+var i = 0;
+while (i < 100) {
+  setTextBId(id, i);
+  i += 1;
+}
