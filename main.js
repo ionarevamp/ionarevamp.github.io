faf9dda6325a6e3e731c75617fcf2e1b8308e231
Iona Erofeeff <37254166+ionarevamp@users.noreply.github.com>
2025-04-07 11:12:38 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-current_doc = document.body.innerHTML;
-document.body.innerHTML = current_doc + "HELLO!"
+function addDiv(id: string, text: string) {
+  const parentElement = document.getElementById(id);
+  const newElement = document.createElement("div");
+  newElement.textContent = text;
+
+  parentElement.appendChild(newElement);
+}
+
+//current_doc = document.body.innerHTML;
+//document.body.innerHTML = current_doc + "HELLO!"
+addDiv("test", "HELLO!");
